5dcc76dd55b5d91f3cf4373b0cd2bde6993d3214
leandro <leandro.lopes.dev@gmail.com>
2020-04-10 19:57:43 -0300

    Projeto com cadastro e validações

---
+++ introducao-javascript/js/calcula-imc.js
@@ -0,0 +1,74 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+// Calcula o IMC
+
+for(var i = 0 ; i < pacientes.length ; i++){
+
+var paciente = pacientes[i];
+
+
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = validaPeso(peso); // true or false
+var alturaEhValida = validaAltura(altura);
+
+
+if(!pesoEhValido){
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
+}
+
+if (!alturaEhValida){
+    alturaEhValida = false;
+    tdImc.textContent = "Altura Inválida";
+    paciente.classList.add("paciente-invalido");
+}
+
+
+if(pesoEhValido && alturaEhValida){
+
+    var imc = calculaImc(peso , altura);
+    tdImc.textContent = imc;
+
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.00){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+}
+
+function calculaImc(peso , altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+
+
