435d2ad654f9834a7aa9d46fd0d0904f53246409
leandro <leandro.lopes.dev@gmail.com>
2020-04-14 17:27:01 -0300

    Javascript avançado I com orientação a objetos e padrões de projetos

---
+++ JQuery parte I/public/js/exec.js
@@ -0,0 +1,18 @@
+function pulaLinha(){
+    document.write("</br>");
+}
+
+
+var anoAtual = 2020;
+var descBrasil = 1500;
+
+
+function media( media){
+
+    media = (anoAtual - descBrasil) / 28 
+
+    pulaLinha()
+
+}
+
+media();
